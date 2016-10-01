688b4768958262406c6044a01de6a8dcab6ec33f
skywindzz <skywindzz@yahoo.com>
2016-09-30 18:37:41 -0700

    sep 30

--- js/grayscale.js
+++ js/grayscale.js
@@ -20,10 +20,29 @@ $(function() {
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+        event.preventDefault(); 
     });
 });
 
+$(function() {
+    $('a.project-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top+200
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault(); 
+    });
+});
+
+$(function() {
+    $('a.test-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top+100
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault(); 
+    });
+});
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
